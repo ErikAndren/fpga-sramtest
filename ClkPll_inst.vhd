d12e86b22d6e2fff5a66301ac26964cc4dcb7f0b
Erik Zachrisson <erik.andren@gmail.com>
2013-09-13 07:25:07 +0200

    Add pll and reset synchronizer

---
+++ ClkPll_inst.vhd
@@ -0,0 +1,6 @@
+ClkPll_inst : ClkPll PORT MAP (
+		areset	 => areset_sig,
+		inclk0	 => inclk0_sig,
+		c0	 => c0_sig,
+		locked	 => locked_sig
+	);
--- SramTest.vhd
+++ SramTest.vhd
@@ -6,19 +6,37 @@ use work.Types.all;
 
 entity SramTest is 
 port (
-	Clk : in bit1;
+	RootClk    : in bit1;
 	ARst_N : in bit1;
 	--
-	Led0 : out bit1;
-	Led1 : out bit1;
-	Led2 : out bit1;
-	Led3 : out bit1
+	Led0   : out bit1;
+	Led1   : out bit1;
+	Led2   : out bit1;
+	Led3   : out bit1
 );
 end entity SramTest;
 
 architecture rtl of SramTest is
-begin
+	signal ARst : bit1;
+	signal Clk : bit1;
+	signal Rst_N : bit1;
 	
-
+begin
+	RstSync : entity work.ResetSync
+		port map (
+			Clk      => Clk,
+         AsyncRst => ARst_N,
+         --
+         Rst_N    => Rst_N
+		);
+        
+	ARst <= not ARst_N;
+   ClkPll0 : entity work.ClkPll
+	Port map (
+		inclk0 => RootClk,
+      areset => ARst,
+      c0     => Clk,
+      Locked => open
+   );
 end architecture;
 
